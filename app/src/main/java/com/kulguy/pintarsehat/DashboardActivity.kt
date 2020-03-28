f27b46e78a63204f342061f8382dbfddf92415d4
komangyogananda <yoganandamahaputra@gmail.com>
2020-03-28 17:11:39 +0800

    Search Component - Search Fragment

--- app/src/main/java/com/kulguy/pintarsehat/DashboardActivity.kt
+++ app/src/main/java/com/kulguy/pintarsehat/DashboardActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.WindowManager
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
@@ -11,6 +12,7 @@ import androidx.fragment.app.FragmentManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.dashboard_content.*
@@ -23,6 +25,7 @@ class DashboardActivity : AppCompatActivity() {
     private val fragment1: Fragment = SearchFragment()
     private val fragment2: Fragment = PhotoFragment()
     private val fragment3: Fragment = DailyNutritionFragment()
+    private var activeFragment: Fragment = fragment1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +44,31 @@ class DashboardActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
-        bottom_navigation.menu.getItem(1).isChecked = true
+        bottom_navigation.menu.getItem(0).isChecked = true
+        activeFragment = fragment1
         fragmentManager.beginTransaction().add(dashboard_content.id, fragment3, "fragment3").hide(fragment3).commit()
-        fragmentManager.beginTransaction().add(dashboard_content.id, fragment2, "fragment2").commit()
-        fragmentManager.beginTransaction().add(dashboard_content.id, fragment1, "fragment1").hide(fragment1).commit()
+        fragmentManager.beginTransaction().add(dashboard_content.id, fragment2, "fragment2").hide(fragment2).commit()
+        fragmentManager.beginTransaction().add(dashboard_content.id, fragment1, "fragment1").commit()
+
+        bottom_navigation.setOnNavigationItemSelectedListener(mBottomNavigationOnNavigationSelectedListener)
+    }
+
+    private val mBottomNavigationOnNavigationSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId){
+            R.id.navigation_photo -> {
+                fragmentManager.beginTransaction().hide(activeFragment).show(fragment2).commit()
+                activeFragment = fragment2
+            }
+            R.id.navigation_nutrionist -> {
+                fragmentManager.beginTransaction().hide(activeFragment).show(fragment1).commit()
+                activeFragment = fragment1
+            }
+            R.id.navigation_daily_nutrition -> {
+                fragmentManager.beginTransaction().hide(activeFragment).show(fragment3).commit()
+                activeFragment = fragment3
+            }
+        }
+        return@OnNavigationItemSelectedListener true
     }
 
     private fun signOut() {
